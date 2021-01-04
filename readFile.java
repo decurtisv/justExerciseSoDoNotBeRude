181b80e20e0d0e121ca61795241cf227637ad9c6
Vincenza De Curtis <53280545+decurtisv@users.noreply.github.com>
2021-01-04 20:46:10 +0100

    Create readFile.java
    [EN]: reading a text file in Java
    [IT]: lettura un file di testo in Java

---
+++ readFile.java
@@ -0,0 +1,30 @@
+/*
+  [EN]: reading a text file in Java
+  [IT]: lettura un file di testo in Java
+*/
+
+import java.io.BufferedReader;    // libreria per leggere i file
+import java.io.FileReader;    // libreria per leggere i file
+import java.io.IOException;
+
+
+public class read {
+
+  public static void main(String[] args) throws IOException{
+    
+    FileReader file = new FileReader("test.txt");
+    BufferedReader lettore = new BufferedReader(file);
+    
+    String linea = lettore.readLine();
+    
+    while(linea != null){
+       System.out.println(linea);
+       linea = lettore.readLine();
+    
+    }
+    
+    file.close(file);
+    
+  }
+}
+
